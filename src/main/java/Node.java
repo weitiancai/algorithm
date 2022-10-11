71011e32747938b37315287ab98a826e3e9f920b
wmg <wmg@servyou.com.cn>
2022-10-12 00:21:44 +0800

    111

---
+++ src/main/java/Node.java
@@ -0,0 +1,18 @@
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
---
+++ src/main/java/ThreeStar.java
@@ -0,0 +1,116 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ThreeStar {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 4, 5, 1, 2,6,7,8,9};
+        System.out.println(getMaxTree(arr));
+    }
+
+    public static Node getMaxTree(int[] arr) {
+        Node[] nArr = new Node[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nArr[i] = new Node(arr[i]);
+        }
+        Stack<Node> stack = new Stack<>();
+        HashMap<Node,Node> lBigMap = new HashMap<>();
+        HashMap<Node,Node> rBigMap = new HashMap<>();
+        for (int i = 0; i != nArr.length; i++) {
+            Node curNode = nArr[i];
+            while ((!stack.isEmpty()) && stack.peek().value < curNode.value) {
+                popStackSetMap(stack, lBigMap);
+            }
+            stack.push(curNode);
+        }
+        while (!stack.isEmpty()) {
+            popStackSetMap(stack, lBigMap);
+        }
+        for (int i = nArr.length - 1; i != -1; i--) {
+            Node curNode = nArr[i];
+            while ((!stack.isEmpty()) && stack.peek().value < curNode.value) {
+                popStackSetMap(stack, rBigMap);
+            }
+            stack.push(curNode);
+        }
+        while (!stack.isEmpty()) {
+            popStackSetMap(stack, rBigMap);
+        }
+        Node head = null;
+        for (int i = 0; i != nArr.length; i++) {
+            Node curNode = nArr[i];
+            Node left = lBigMap.get(curNode);
+            Node right = rBigMap.get(curNode);
+            if (left == null && right == null) {
+                head = curNode;
+            } else if (left == null) {
+                if (right.left == null) {
+                    right.left = curNode;
+                } else {
+                    right.right = curNode;
+                }
+            } else if (right == null) {
+                if (left.left == null) {
+                    left.left = curNode;
+                } else {
+                    left.right = curNode;
+                }
+            } else {
+                Node parent = left.value < right.value ? left : right;
+                if (parent.left == null) {
+                    parent.left = curNode;
+                } else {
+                    parent.right = curNode;
+                }
+            }
+        }
+        return head;
+    }
+
+    private static void popStackSetMap(Stack<Node> stack, HashMap<Node, Node> map) {
+        Node popNode = stack.pop();
+        if (stack.isEmpty()) {
+            map.put(popNode, null);
+        } else {
+            map.put(popNode, stack.peek());
+        }
+    }
+
+    public static int maxRecSize(int[][] map) {
+        if (map == null || map.length == 0 || map[0].length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        int[] height = new int[map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                height[j] = map[i][j] == 0 ? height[j] : height[j] + 1;
+            }
+            maxArea = Math.max(maxRecFromBottom(height), maxArea);
+        }
+        return maxArea;
+    }
+
+    private static int maxRecFromBottom(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] <= height[stack.peek()]) {
+                int j = stack.pop();
+                int k = stack.isEmpty()? -1 : stack.peek();
+                int currArea = (i - k - 1) * height[j];
+                maxArea = Math.max(maxArea, currArea);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int k = stack.isEmpty()? -1: stack.peek();
+            int curArea = (height.length - k - 1) * height[j];
+            maxArea = Math.max(maxArea, curArea);
+        }
+        return maxArea;
+    }
+}
