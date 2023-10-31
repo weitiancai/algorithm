e4a8e6d73ad3271a5a876d343dec92ca406cf3f2
wmg <wmg@servyou.com.cn>
2023-10-31 11:17:42 +0800

    插入排序

--- src/main/java/sort/maopaoOrInsert.java
+++ src/main/java/sort/maopaoOrInsert.java
@@ -1,13 +1,15 @@
 package sort;
 
+import java.util.Arrays;
+
 public class maopaoOrInsert {
     public static void main(String[] args) {
         int[] arr = new int[] {435,8,24,2,9,456,45,63,34};
-        mpsort(arr);
+//        mpsort(arr);
 //        insertSort(arr);
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
     // 插入排序，平均时间复杂度 o(n的平方）
     // 本身有序的情况下， 最佳排序复杂度 o(n)
@@ -35,6 +37,21 @@ public class maopaoOrInsert {
                     arr[j+1] = tmp;
                 }
             }
+        }
+    }
+
+    private static void selectionSort(int[] arr) {
+        int n= arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int k = i;
+            for (int j = i + 1; j < n; j++) {
+                if(arr[k] > arr[j]){
+                    k = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = tmp;
         }
     }
 }
