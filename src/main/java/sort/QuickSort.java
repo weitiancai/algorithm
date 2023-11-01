74de3c9b0c3b260fc342148254448a95f73d066e
wmg <wmg@servyou.com.cn>
2023-11-01 21:05:41 +0800

    quicksort

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -3,42 +3,6 @@ package sort;
 import java.util.Arrays;
 
 public class QuickSort {
-//    public static void quickSort(int[] arr, int low, int high) {
-//        int i,j,temp,t;
-//        if(low>high){
-//            return;
-//        }
-//        i=low;
-//        j=high;
-//        //temp就是基准位
-//        temp = arr[low];
-//
-//        while (i<j) {
-//            //先看右边，依次往左递减
-//            while (temp<=arr[j]&&i<j) {
-//                j--;
-//            }
-//            //再看左边，依次往右递增
-//            while (temp>=arr[i]&&i<j) {
-//                i++;
-//            }
-//            //如果满足条件则交换
-//            if (i<j) {
-//                t = arr[j];
-//                arr[j] = arr[i];
-//                arr[i] = t;
-//            }
-//
-//        }
-//        //最后将基准为与i和j相等位置的数字交换
-//        arr[low] = arr[i];
-//        arr[i] = temp;
-//        //递归调用左半数组
-//        quickSort(arr, low, j-1);
-//        //递归调用右半数组
-//        quickSort(arr, j+1, high);
-//
-//    }
 
     // 快速排序
     // 平均 o(logn)
@@ -71,7 +35,36 @@ public class QuickSort {
     }
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 4, 7, 1, 3};
-        QuickSort.quickSort(arr, 0, arr.length - 1);
+        QuickSort.myQuickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void myQuickSort(int[] arr, int left, int right){
+        if(left>=right)return;
+
+        int n = arr.length;
+        int i = left,j = right;
+        int pivot = arr[right];
+
+
+        while(i<j){
+            while(i < j && arr[i] < pivot){
+                i++;
+            }
+            if(i<j) {
+                arr[j--] = arr[i];
+            }
+            while(i < j && arr[j] > pivot){
+                j--;
+            }
+            if(i<j) {
+                arr[i++] = arr[j];
+            }
+        }
+
+
+        myQuickSort( arr, 0, i-1);
+        myQuickSort( arr, i+1,right);
     }
 }
