45f850265c6ba5ebd181f318691b02d02cff0951
wmg <wmg@servyou.com.cn>
2023-12-24 20:12:23 +0800

    两数合并递归法

--- src/test/java/testAlgorithm.java
+++ src/test/java/testAlgorithm.java
@@ -2498,4 +2498,18 @@ public class testAlgorithm {
         }
         return vitualNode.next;
     }
+    // 递归法
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        } else if(list2 == null){
+            return list1;
+        } else if (list1.val <= list2.val){
+            list1.next = mergeTwoLists2(list1.next, list2);
+            return list1;
+        } else{
+            list2.next = mergeTwoLists2(list1,list2.next);
+            return list2;
+        }
+    }
 }
